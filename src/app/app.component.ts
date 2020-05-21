c7c8bea818a1a4ae0b1c5ce12e6bb27af01f6e64
Matt Luffman <matt.luffman@gmail.com>
2020-05-21 18:03:43 -0400

    lectures 68 - 72

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,23 @@ import {Component} from '@angular/core';
 export class AppComponent {
     serverElements = [{type: 'server', name: 'testserver', content: 'this is just a test'}];
 
+    onServerAdded(serverData: { serverName: string, serverContent: string }) {
+        console.log("in onServerAdded()");
+        console.log(serverData);
+        this.serverElements.push({
+            type: 'server',
+            name: serverData.serverName,
+            content: serverData.serverContent
+        });
+    }
 
+    onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+        console.log("in onBlueprintAdded()");
+        console.log(blueprintData);
+        this.serverElements.push({
+            type: 'blueprint',
+            name: blueprintData.serverName,
+            content: blueprintData.serverContent
+        });
+    }
 }
