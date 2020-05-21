73f7f37d2b971c25d99bdfb9cb1ea0d24aca6a9e
Matt Luffman <matt.luffman@gmail.com>
2020-05-21 18:44:26 -0400

    lectures 74 & 75 template variables

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-cockpit',
@@ -9,7 +9,8 @@ export class CockpitComponent implements OnInit {
     @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string; }>();
     @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string; }>();
     // newServerName = '';
-    newServerContent = '';
+    // newServerContent = '';
+    @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
 
     constructor() {
     }
@@ -22,21 +23,23 @@ export class CockpitComponent implements OnInit {
         console.log(nameInput.value);
         this.serverCreated.emit({
             serverName: nameInput.value,
-            serverContent: this.newServerContent
+            serverContent: this.serverContentInput.nativeElement.value
         });
         this.clearFields();
     }
 
     onAddBlueprint(nameInput: HTMLInputElement) {
+        // trying out what is in serverContentInput
+        console.log(this.serverContentInput);
         this.blueprintCreated.emit({
             serverName: nameInput.value,
-            serverContent: this.newServerContent
+            serverContent: this.serverContentInput.nativeElement.value
         });
         this.clearFields();
     }
 
     private clearFields(): void {
-        this.newServerContent = '';
+        // this.newServerContent = '';
         // this.newServerName = '';
     }
 }
