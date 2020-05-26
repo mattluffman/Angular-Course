1e7c200b455481f76d87f4973ce6623cfc74a769
Matt Luffman <matt.luffman@gmail.com>
2020-05-26 18:06:40 -0400

    assignment-4 start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,33 +6,4 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    serverElements = [{type: 'server', name: 'testserver', content: 'this is just a test'}];
-
-    onServerAdded(serverData: { serverName: string, serverContent: string }): void {
-        console.log("in onServerAdded()");
-        console.log(serverData);
-        this.serverElements.push({
-            type: 'server',
-            name: serverData.serverName,
-            content: serverData.serverContent
-        });
-    }
-
-    onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }): void {
-        console.log("in onBlueprintAdded()");
-        console.log(blueprintData);
-        this.serverElements.push({
-            type: 'blueprint',
-            name: blueprintData.serverName,
-            content: blueprintData.serverContent
-        });
-    }
-
-    onChangeFirst(): void {
-        this.serverElements[0].name = 'Changed';
-    }
-
-    onDestroyFirst() {
-        this.serverElements.splice(0, 1);
-    }
 }
