e0f343e7a537096e0d259a2057f5d9d94a743014
Matt Luffman <matt.luffman@gmail.com>
2021-03-08 14:32:09 -0500

    lecture 99 - adding properties to the directive & letting the user of the directive define what the colors are.

--- src/app/better-highlight/better-hightlight.directive.ts
+++ src/app/better-highlight/better-hightlight.directive.ts
@@ -1,25 +1,28 @@
-import {Directive, HostBinding, HostListener, OnInit} from '@angular/core';
+import {Directive, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 
 @Directive({
     selector: '[appBetterHightlight]'
 })
 export class BetterHightlightDirective implements OnInit {
-    @HostBinding('style.backgroundColor') backgroundColor = 'transparent';
+    @Input() defaultColor: string = 'transparent';
+    @Input() highlightColor: string = 'blue';
+    @HostBinding('style.backgroundColor') backgroundColor;
 
     constructor() {
     }
 
     ngOnInit(): void {
         // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
+        this.backgroundColor = this.defaultColor;
     }
 
     @HostListener('mouseenter') mouseover(eventData: Event) {
         // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'blue');
-        this.backgroundColor = 'blue';
+        this.backgroundColor = this.highlightColor;
     }
 
     @HostListener('mouseleave') mouseleave(eventData: Event) {
         // this.renderer.setStyle(this.element.nativeElement, 'background-color', 'transparent');
-        this.backgroundColor = 'transparent';
+        this.backgroundColor = this.defaultColor;
     }
 }
