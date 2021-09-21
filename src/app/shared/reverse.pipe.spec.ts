32959a1bc9a20b43ae10f3ef7aba81ae9339ffd7
Matt Luffman <matt.luffman@gmail.com>
2021-09-21 18:26:47 -0400

    section 28, lecture 428 - isolated tests for pipes

--- src/app/shared/reverse.pipe.spec.ts
+++ src/app/shared/reverse.pipe.spec.ts
@@ -2,10 +2,10 @@
 
 import {ReversePipe} from './reverse.pipe';
 
+// example of isolated test. No need for test bed, etc.
 describe('Pipe: ReversePipe', () => {
     it('should reverse the inputs', () => {
-        let reversePipe = new ReversePipe();
+        const reversePipe = new ReversePipe();
         expect(reversePipe.transform('hello')).toEqual('olleh');
     });
-
 });
